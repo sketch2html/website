7feef21776138af5aea659865cd650bb8420a999
army8735 <army8735@qq.com>
2018-11-09 15:16:00 +0800

    basic数据

--- app/assets/layout_basic/LayoutBasic.jsx
+++ app/assets/layout_basic/LayoutBasic.jsx
@@ -9,13 +9,21 @@ class LayoutBasic extends migi.Component {
   }
 
   @bind list
-  @bind direction
-  @bind num
   @bind dis
+  @bind direction
+  @bind num = 2
+  @bind typeConsistency = 0.5
+  @bind widthConsistency = 0.5
+  @bind heightConsistency = 0.5
+  @bind marginConsistency = 0.5
+  @bind startConsistency = 0.5
+  @bind centerConsistency = 0.5
+  @bind endConsistency = 0.5
+  @bind fontConsistency = 0.5
+  @bind lineConsistency = 0.5
 
   gen() {
     this.list = [];
-    this.num = 2 + Math.floor(Math.random() * 3);
     let a = {
       type: Math.random() > 0.5 ? 1 : 0,
       width: 20 + Math.floor(Math.random() * 100),
@@ -37,33 +45,29 @@ class LayoutBasic extends migi.Component {
     for(let i = 1; i < this.num; i++) {
       let last = this.list[this.list.length - 1];
       let b = {
-        type: Math.random() > 0.2 ? a.type : (Math.random() > 0.5 ? 1 : 0),
-        width: Math.random() > 0.5 ? a.width : (20 + Math.floor(Math.random() * 100)),
-        height: Math.random() > 0.5 ? a.height : (20 + Math.floor(Math.random() * 100)),
+        type: Math.random() > this.typeConsistency ? a.type : (Math.random() > 0.5 ? 1 : 0),
+        width: Math.random() > this.widthConsistency ? a.width : (20 + Math.floor(Math.random() * 100)),
+        height: Math.random() > this.heightConsistency ? a.height : (20 + Math.floor(Math.random() * 100)),
       };
       if(b.type) {
-        b.fontSize = a.fontSize && Math.random() > 0.2 ? a.fontSize : 10 + Math.ceil(Math.random() * 20);
-        b.lineHeight = a.fontSize && Math.random() > 0.2 ? a.lineHeight : 1 + Math.floor(Math.random() * 3);
+        b.fontSize = a.fontSize && Math.random() > this.fontConsistency ? a.fontSize : 10 + Math.ceil(Math.random() * 20);
+        b.lineHeight = a.fontSize && Math.random() > this.lineConsistency ? a.lineHeight : 1 + Math.floor(Math.random() * 3);
       }
       else {
         b.fontSize = 0;
         b.lineHeight = 0;
       }
-      let start = Math.random() > 0.5;
-      let center = Math.random() > 0.5;
-      let end = Math.random() > 0.5;
+      let start = Math.random() > this.startConsistency;
+      let center = Math.random() > this.centerConsistency;
+      let end = Math.random() > this.endConsistency;
       if(start && center || center && end || start && end) {
         start = center = end = true;
       }
       if(this.direction) {
-        b.x = last.x + last.width + (Math.random() > 0.2 ? margin : (10 + Math.floor(Math.random() * 100)));
+        b.x = last.x + last.width + (Math.random() > this.marginConsistency ? margin : (10 + Math.floor(Math.random() * 100)));
         if(start && end) {
           b.y = a.y;
           b.height = a.height;
-          if(a.fontSize) {
-            b.fontSize = a.fontSize;
-            b.lineHeight = a.lineHeight;
-          }
         }
         else if(start) {
           b.y = a.y;
@@ -80,20 +84,19 @@ class LayoutBasic extends migi.Component {
           if(b.height <= a.height) {
             b.y = (a.height - b.height) * 0.5;
           }
+          else {
+            b.y = a.y;
+          }
         }
         else {
           b.y = Math.floor(Math.random() * 20);
         }
       }
       else {
-        b.y = last.y + last.height + (Math.random() > 0.2 ? margin : (10 + Math.floor(Math.random() * 100)));
+        b.y = last.y + last.height + (Math.random() > this.marginConsistency ? margin : (10 + Math.floor(Math.random() * 100)));
         if(start && end) {
           b.x = a.x;
           b.width = a.width;
-          if(a.fontSize) {
-            b.fontSize = a.fontSize;
-            b.lineHeight = a.lineHeight;
-          }
         }
         else if(start) {
           b.x = a.x;
@@ -109,6 +112,9 @@ class LayoutBasic extends migi.Component {
         else if(center) {
           if(b.width <= a.width) {
             b.x = (a.width - b.width) * 0.5;
+          }
+          else {
+            b.x = a.x;
           }
         }
         else {
@@ -156,6 +162,18 @@ class LayoutBasic extends migi.Component {
   }
   render() {
     return <div class="g-wrap layout-basic">
+      <div class="config">
+        <label>数量</label><input type="range" min="2" max="5" step="1" value={this.num} />
+        <label>类型一致性</label><input type="range" min="0" max="1" step="0.1" value={this.typeConsistency} />
+        <label>宽度一致性</label><input type="range" min="0" max="1" step="0.1" value={this.widthConsistency} />
+        <label>高度一致性</label><input type="range" min="0" max="1" step="0.1" value={this.heightConsistency} />
+        <label>边距一致性</label><input type="range" min="0" max="1" step="0.1" value={this.marginConsistency} />
+        <label>开始一致性</label><input type="range" min="0" max="1" step="0.1" value={this.startConsistency} />
+        <label>中间一致性</label><input type="range" min="0" max="1" step="0.1" value={this.centerConsistency} />
+        <label>结尾一致性</label><input type="range" min="0" max="1" step="0.1" value={this.endConsistency} />
+        <label>字大一致性</label><input type="range" min="0" max="1" step="0.1" value={this.fontConsistency} />
+        <label>行高一致性</label><input type="range" min="0" max="1" step="0.1" value={this.lineConsistency} />
+      </div>
       <div class="btn">
         <button disabled={this.dis} onClick={this.clickY}>成组</button>
         <button disabled={this.dis} onClick={this.clickN}>不成组</button>
